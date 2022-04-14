364c04deede1d9ed361431e830e22aac283ba4d3
yatharth811 <yatharth.bansiwal@gmail.com>
2022-04-14 19:29:47 +0530

    Testing

---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
+    }
+  }
+}
